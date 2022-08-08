63d86a0938beff40ecdc3f0eefe0d754fe5e3211
jeffAmerson <aliamer19ali@gmail.com>
2022-08-08 11:06:06 +0300

    correcting a typo

--- src/_DATA_/aboutMe.ts
+++ src/_DATA_/aboutMe.ts
@@ -2,6 +2,6 @@ export const aboutMe: { subTitle: string; story: string; arther: string } = {
   subTitle:
     "I've been training for more than two years to become a successful web developer and I still want to learn more because I believe that websites are the future.",
   story:
-    "I'm Full stack developer with a passion for learning new technologies and working with teams to create user-friendly web applications. I have a lot of experience in web development, including front-end and back-end technologies. Eager to develop skills in a fast-paced environment and contribute to the success of a company.",
+    "I'm Full stack developer with a passion for learning new technologies and working with team to create user-friendly web applications. I have a lot of experience in web development, including front-end and back-end technologies. Eager to develop skills in a fast-paced environment and contribute to the success of a company.",
   arther: "Ali Amer",
 };
